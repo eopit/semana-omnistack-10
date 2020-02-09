d7d6b12577e3ba92fe863a039409999130424ee2
eopit <pedro.barricelli@gmail.com>
2020-02-09 15:40:58 -0300

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const routes = require('./routes');
+
+mongoose.connect('mongodb+srv://eopit:eopit123@cluster-1-lii04.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+//Quando eh utilizado o 
+app.use(express.json());
+app.use(routes);
+// MÉTODOS HTTP
+    //get, post, put, delete
+    //get -> pega informacao, listar, etc
+
+    //post -> criar alguma coisa, salvar, etc
+
+    //put -> editar um usuario
+
+    //delete -> deletar algo
+
+    
+// TIPO DE PARAMETROS
+    // Query Params -> req.query (filtros, ordenacao, etc)
+
+    // Route Params -> req.params identificar recurso na alteracao ou remocao
+
+    // Body -> req.body criacao ou alteracao de registyro
+
+app.listen(3333);
